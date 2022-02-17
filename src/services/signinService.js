d8cc72993c816e61ac098802927357730c70d7c1
Nyaksik <makapori@gmail.com>
2022-02-17 12:51:33 +0700

    fix

--- src/services/signinService.js
+++ src/services/signinService.js
@@ -4,17 +4,16 @@ import TokenService from "./tokenService"
 export default function SigninService() {
     this.baseHeader = () => {
         return {
-            'Content-Type': 'application/json',
             'X-APP-ID': new TokenService().getToken(DEVICE)
         }
     }
 
     this.signinUser = async (data) => {
         const formData = new FormData()
-
-        formData.append("username", data.username)
-		formData.append("password", data.password)
-
+        
+        formData.set("username", data.username)
+		formData.set("password", data.password)
+        
         const res = await fetch(`${URL}signin`, {
             method: 'POST',
             headers: this.baseHeader(),
